93c25793d60dc9828de3d57085096e64e2ff7b13
Fabien <fabien.sens@free.fr>
2024-09-09 12:22:19 +0200

    Show issue details

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -12,7 +12,7 @@ const IssuesPage = async () => {
 
   return (
     <div>
-      <IssueActions/>
+      <IssueActions />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -29,7 +29,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
