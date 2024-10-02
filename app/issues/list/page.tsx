c64b9563c65d482189c2d323b2dce26464fbec8d
Fabien <fabien.sens@free.fr>
2024-10-02 11:40:09 +0200

    Build the filter component

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,6 +2,7 @@ import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssueActions from "./IssueActions";
+import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
