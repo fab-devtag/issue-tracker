b4435a1f23c51fa687e632d32dc9d3d772a36575
Fabien <fabien.sens@free.fr>
2024-10-03 13:27:47 +0200

    Refactor - Extract Issue Table

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,37 +1,24 @@
-import { IssueStatusBadge, Link, Pagination } from "@/app/components";
+import { Pagination } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Table } from "@radix-ui/themes";
+import { Status } from "@prisma/client";
 import IssueActions from "./IssueActions";
-import IssueStatusFilter from "./IssueStatusFilter";
-import { Issue, Status } from "@prisma/client";
-import NextLink from "next/link";
-import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
+import IssueTable, { columnNames, IssueQuery } from "./IssueTable";
+import { Flex } from "@radix-ui/themes";
 
 interface Props {
-  searchParams: {
-    status: Status;
-    orderBy: keyof Issue;
-    sortOrder: "asc" | "desc";
-    page: string;
-  };
+  searchParams: IssueQuery;
 }
+
 const IssuesPage = async ({ searchParams }: Props) => {
-  const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: "Issues", value: "title" },
-    { label: "Status", value: "status", className: "hidden md:table-cell" },
-    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
-  ];
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
 
-  console.log(status);
-
   const where = { status: status };
 
-  const orderBy = columns
-    .map((column) => column.value)
+  const orderBy = columnNames
+    .map((column) => column)
     .includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: searchParams.sortOrder }
     : undefined;
@@ -51,67 +38,15 @@ const IssuesPage = async ({ searchParams }: Props) => {
   });
 
   return (
-    <div>
+    <Flex direction='column' gap='3'>
       <IssueActions />
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((column) => {
-              const columnIsActive = searchParams.orderBy === column.value;
-              const isAsc = searchParams.sortOrder === "asc";
-
-              return (
-                <Table.ColumnHeaderCell
-                  key={column.value}
-                  className={column.className}
-                >
-                  <NextLink
-                    href={{
-                      query: {
-                        ...searchParams,
-                        orderBy: column.value,
-                        sortOrder: columnIsActive && isAsc ? "desc" : "asc",
-                      },
-                    }}
-                  >
-                    {column.label}
-                  </NextLink>
-                  {columnIsActive &&
-                    (isAsc ? (
-                      <ArrowUpIcon className="inline" />
-                    ) : (
-                      <ArrowDownIcon className="inline" />
-                    ))}
-                </Table.ColumnHeaderCell>
-              );
-            })}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <IssueTable searchParams={searchParams} issues={issues} />
       <Pagination
         pageSize={pageSize}
         currentPage={page}
         itemCount={issueCount}
       />
-    </div>
+    </Flex>
   );
 };
 
