847063c8182bf9001e8d13dfd0c33b4c1511ae0a
Fabien <fabien.sens@free.fr>
2024-10-03 12:07:34 +0200

    Fix Filtering Bugs

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value?: Status }[] = [
@@ -14,18 +14,30 @@ const statuses: { label: string; value?: Status }[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || "ALL"}
       onValueChange={(status) => {
-        const query = status !== "All" ? `?status=${status}` : "";
+        const params = new URLSearchParams();
+        if (status !== "ALL") params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+        if (searchParams.get("sortOrder"))
+          params.append("sortOrder", searchParams.get("sortOrder")!);
+
+        const query = params.size ? `?${params.toString()}` : "";
         router.push(`/issues/list${query}`);
       }}
     >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
-          <Select.Item key={status.value} value={status.value || "All"}>
+          <Select.Item
+            key={status.value || "ALL"}
+            value={status.value || "ALL"}
+          >
             {status.label}
           </Select.Item>
         ))}
